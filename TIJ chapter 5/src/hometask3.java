36a6abc85ced9bd059b247cb4919529b0bee4426
Ткалин Владимир <tkalin.vladimir@gmail.com>
2016-10-04 17:04:03 +1000

    класс с конструктором по умолчанию без параметров выводящий сообщение

---
+++ TIJ chapter 5/src/hometask3.java
@@ -0,0 +1,10 @@
+public class hometask3 {
+    hometask3() {
+        System.out.println("Я конструктор без параметров");
+    }
+
+    public static void main(String[] args) {
+        hometask3 class_1 = new hometask3();
+    }
+
+}
