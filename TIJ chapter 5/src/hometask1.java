4f118103cf292006427b5f7987d304adb51e4161
Ткалин Владимир <tkalin.vladimir@gmail.com>
2016-10-04 10:54:04 +1000

    класс с неиницилизированной ссылкой на стринг. стринг = нулл.

---
+++ TIJ chapter 5/src/hometask1.java
@@ -0,0 +1,15 @@
+public class hometask1 {
+
+    public static void main(String[] args) {
+
+        test test1 = new test();
+        System.out.println(test1.s);
+    }
+
+}
+
+
+class test {
+
+    String s;
+}
