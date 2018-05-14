d971ee9d94623e708f947c1a68c4da1d8c093a26
Ткалин Владимир <tkalin.vladimir@gmail.com>
2018-05-14 12:51:14 +1000

    3608 - MVC

---
+++ JR homework/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task36.task3608;
+
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.MainModel;
+import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
+import com.javarush.task.task36.task3608.view.UsersView;
+
+public class Solution {
+    public static void main(String[] args) {
+        Model model = new MainModel();
+        UsersView usersView = new UsersView();
+        Controller controller = new Controller();
+        EditUserView editUserView = new EditUserView();
+        controller.setEditUserView(editUserView);
+        editUserView.setController(controller);
+
+        usersView.setController(controller);
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+
+        usersView.fireEventShowAllUsers();
+        usersView.fireEventOpenUserEditForm(126);
+        editUserView.fireEventUserDeleted(124);
+        editUserView.fireEventUserChanged("Vova", 126, 10);
+        usersView.fireEventShowDeletedUsers();
+    }
+}
---
+++ JR homework/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/ModelData.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task36.task3608.model;
+
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.List;
+
+public class ModelData {
+    public boolean isDisplayDeletedUserList() {
+        return displayDeletedUserList;
+    }
+
+    public void setDisplayDeletedUserList(boolean displayDeletedUserList) {
+        this.displayDeletedUserList = displayDeletedUserList;
+    }
+
+    private boolean displayDeletedUserList;
+
+    public User getActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(User activeUser) {
+        this.activeUser = activeUser;
+    }
+
+    private User activeUser;
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
