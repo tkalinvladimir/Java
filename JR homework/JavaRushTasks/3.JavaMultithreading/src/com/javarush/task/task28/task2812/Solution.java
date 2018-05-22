66cbcc7d653fa80db15cec2d5aa44cc60c63656d
Ткалин Владимир <tkalin.vladimir@gmail.com>
2018-05-23 09:54:13 +1000

    2606 - newFixedThreadPool 2807 -  ++ LinkedBlockingQueue 2812 - вывод невыполненных задач

---
+++ JR homework/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task28/task2812/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task28.task2812;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/* 
+ShutdownNow!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 1; i <= 10; i++) {
+            final int localId = i;
+            executor.submit(new Runnable() {
+                public void run() {
+                    doExpensiveOperation(localId);
+                }
+            });
+        }
+
+        ArrayList<Runnable> tasks = (ArrayList<Runnable>) executor.shutdownNow();
+        for (Runnable r:tasks) {
+            System.out.println(r);
+        }
+    }
+
+    private static void doExpensiveOperation(int localId) {
+        System.out.println(Thread.currentThread().getName() + ", localId="+localId);
+    }
+}
