41307e7eeaffb88c0e7ad4ff5e9e024b144f6c54
Ткалин Владимир <tkalin.vladimir@gmail.com>
2018-05-30 09:37:17 +1000

    3203, 3213, 3202 - StringWriter, StringReader

---
+++ JR homework/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task32/task3203/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task32.task3203;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+
+/*
+Пишем стек-трейс
+*/
+public class Solution {
+    public static void main(String[] args) {
+        String text = getStackTrace(new IndexOutOfBoundsException("fff"));
+        System.out.println(text);
+    }
+
+    public static String getStackTrace(Throwable throwable) {
+        Writer sw = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+}
